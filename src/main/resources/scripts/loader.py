a504ab2c2a81e0c181a994a242bd6ea81e26dd1a
Tomasz Kopacki <tomasz@kopacki.eu>
2012-09-06 13:40:40 +0200

    loader.py

--- src/main/resources/scripts/loader.py
+++ src/main/resources/scripts/loader.py
@@ -1,14 +1,56 @@
 import xml.etree.ElementTree as ET
 import pymysql
 
-
-class ScriptLoader:
-    _xmlPath = '/opt/builder/src/src/main/resources/scripts/main.xml'
-    _Scriptspath = '/opt/builder/src/src/main/resources/scripts/sources'
+class ConfigManager:
+    _path = 'loader.config'
     
     def __init__(self):
-        self._xml = ET.parse(self._xmlPath).getroot()
-        self._connection = pymysql.connect(host='172.16.1.5', port=3306, user='agora', passwd='tomasz.12', db='agora_erp')
+        self._config = ConfigParser.ConfigParser().read(self._path)
+        
+    def getProperty(self, section, option):
+        return self._config.get(section, option)
+
+class DBManager:
+    def __init__(self, config):
+        self._connection = pymysql.connect(host=config.get('db', 'host'), port=3306, user=config.get('db', 'user'), passwd=config.get('db', 'password'), db=config.get('db', 'db'))
+        self._config = config
+        
+    def isScriptAvailable(self, scriptName):
+        pass
+    
+    def getScriptId(self, scriptName):
+        pass
+    
+    def updateScript(self, scriptName, source, onInit):
+        pass
+    
+    def insertScript(self, scriptName, source, onInit):
+        sql = self._config.get('queries' ,'insertScript')
+        cursor = self._connection.cursor()
+        cursor.execute(sql, (scriptName, source, onInit))
+        cursor.connection.commit()
+        user_id = cursor.connection.insert_id()
+        cursor.close()
+        return user_id
+    
+    def deleteScriptScheduler(self, scriptId):
+        pass
+    
+    def deleteScriptDependencies(self, scriptId):
+        pass
+    
+    def insertScriptScheduler(self, scriptId, schedulerName, enabled, fireAt):
+        pass
+        
+    def insertScriptDependency(self, scriptId, dependencyName):
+        pass
+
+class ScriptLoader:
+    
+    def __init__(self):
+        self._config = ConfigManager()
+        self._dbManager = DBManager(self._config)
+        self._xml = ET.parse(self._config.get('paths', 'config')).getroot()
         
     def loadScripts(self):
         for script in self._xml.findall('script'):
@@ -18,25 +60,25 @@ class ScriptLoader:
             schedulerName = script.find('scheduler/name')
             schedulerEnabled = script.find('scheduler/enabled')
             schedulerFireAt = script.find('scheduler/fireAt')
-            id = self.saveScript(self.getText(name), self.getText(source), self.getText(onInit))
-            print id
+            id = self.saveScript(self.getText(name), self.readFile(self.__scriptsPath + self.getText(source)), self.getText(onInit))
             self.saveScheduler(id, self.getText(schedulerName), self.getText(schedulerEnabled), self.getText(schedulerFireAt))
             self.saveDependencies(id, script.findall('dependencies'))
             
     def saveScript(self, name, source, onInit):
-        sql = """INSERT INTO script (name, script, on_init) VALUES (%s, %s, %s)"""
-        cursor = self._connection.cursor()
-        cursor.execute(sql, (name, source, onInit))
-        cursor.connection.commit()
-        user_id = cursor.connection.insert_id()
-        cursor.close()
-        return user_id
+        if self._dbManager.isScriptAvailable(name):
+            self._dbManager.updateScript(name, source, onInit)
+            return self._dbManager.getScriptId(name)
+        else:
+            return self._dbManager.insertScript(name, self.readFile(self._config.get('paths', 'scripts') + source), onInit)
         
     def saveDependencies(self, id, dependencies):
-        print dependencies
+        self._dbManager.deleteScriptDependencies(id)
+        for dependency in dependencies:
+            self._dbManager.insertScriptDependency(id, self.getText(dependency))
                 
     def saveScheduler(self, id, schedulerName, schedulerEnabled, schedulerFireAt):
-        print schedulerName
+        self._dbManager.deleteScriptScheduler(id)
+        self._dbManager.insertScriptScheduler(id, schedulerName, schedulerEnabled, schedulerFireAt)
         
     def getText(self, node):
         if node == None or node.text == None:
@@ -44,6 +86,9 @@ class ScriptLoader:
         else:
             return node.text
         
+    def readFile(self, fullPath):
+        return ''.join(open(fullPath, 'r').readlines())
+    
 loader = ScriptLoader()
 loader.loadScripts()
             
