f3384bc52ce9ecec8993c66d73922f46101ce226
lobo <lobo@lobo.dom.local>
2012-09-09 18:22:29 +0200

    .

--- src/main/resources/scripts/sources/read-ms-access-db.py
+++ src/main/resources/scripts/sources/read-ms-access-db.py
@@ -3,5 +3,5 @@ from java.io import File
 
 class MSAccessReader(Container):
     
-    def __init__(self, dbPath):
+    def __init__(self):
         print Database.open(File(properties.getProperty('msAccessDBPath'))).getTableNames() 
