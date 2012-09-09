c1387e807dc891f2c30ce5c6dbb35e26a8d87b33
lobo <lobo@lobo.dom.local>
2012-09-09 18:46:08 +0200

    .

--- src/main/resources/scripts/sources/read-ms-access-db.py
+++ src/main/resources/scripts/sources/read-ms-access-db.py
@@ -2,6 +2,40 @@ from com.healthmarketscience.jackcess import Database;
 from java.io import File
 
 class MSAccessReader(Container):
+    _sqlCreateTable = """
+        CREATE TABLE `{0}` (
+            `id` int(11) NOT NULL AUTO_INCREMENT,
+            PRIMARY KEY (`id`),
+        ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci AUTO_INCREMENT=1 ;
+        """
+    _sqlAddColumn = """
+        ALTER TABLE `{0}` ADD COLUMN `{1}` VARCHAR(1024);
+    """
+    _sqlInsert = """
+        INSERT INTO `{0}`(`{1}`) VALUES("{2}");
+    """
     
     def __init__(self):
-        print Database.open(File(properties.getProperty('msAccessDBPath'))).getTableNames() 
+        self.clearDatabase()
+        tables = Database.open(File(properties.getProperty('msAccessDBPath'))).getTableNames() 
+        for table in tables:
+            self.processTable(table)
+
+    def processTable(self, table):
+        self.processTableStructure(table)
+        self.processTableData(table)
+
+    def clearDatabase(self):
+        pass
+
+    def processTableStructure(self, table):
+        self.createTable(table.getName())
+        columns = table.getColumns()
+        for column in columns:
+            self.addColumn(column.getName())
+
+    def createTable(self, name):
+        oldEntityManager.createNativeQuery(self._sqlCreateTable % name).executeUpdate()
+
+    def addColumn(self, name):
+        pass
