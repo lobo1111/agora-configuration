e49c4046e132c263ace079af0cfc60bc35c7932b
Tomasz Kopacki <tomasz@kopacki.eu>
2012-09-11 12:51:00 +0200

    .

--- src/main/resources/scripts/sources/sync-access-to-db.py
+++ src/main/resources/scripts/sources/sync-access-to-db.py
@@ -19,7 +19,7 @@ class SyncAccessToDb(Container):
         communities = self.loadData('SELECT w FROM Wspolne w')
         for community in communities:
             self._processed += 1
-            self._logger.info('processing community %s' % unicode(community.getNazwa()), errors = 'replace')
+            self._logger.info('processing community %s' % unicode(community.getNazwa(), errors = 'replace'))
             if self.communityExists(community):
                 self._logger.info('community exists, updating')
                 self.communityUpdate(community)
