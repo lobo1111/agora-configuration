5e9d423c402141a72df6fc4b934c7467795d3175
Tomasz Kopacki <tomasz@kopacki.eu>
2012-09-11 12:02:16 +0200

    .

--- src/main/resources/scripts/sources/sync-access-to-db.py
+++ src/main/resources/scripts/sources/sync-access-to-db.py
@@ -1,4 +1,4 @@
-from pl.reaper.container.data.old import Wspolne
+from pl.reaper.container.data import Community
 
 class SyncAccessToDb(Container):
     def __init__(self):
@@ -12,4 +12,40 @@ class SyncAccessToDb(Container):
     def syncCommunities(self):
         communities = self.loadData('SELECT w FROM Wspolne w')
         for community in communities:
-            print community
+            if self.communityExists(community):
+                self.communityUpdate(community)
+            else:
+                self.communityInsert(community)
+                
+    def communityExists(self, community):
+        return syncDataExists('sync_community', 'erp_community_id', community.getId())
+    
+    def communityUpdate(self, oldCommunity):
+        id = self.findBaseId('sync_community', 'erp_community_id', 'access_community_id', oldCommunity.getId())
+        community = self.findCommunity(id)
+        self.setDataAndPersistCommunity(oldCommunity, community)
+        
+    def findCommunity(self, id):
+        return entityManager.createQuery(('SELECT c FROM Community c WHERE c.id = %d' % id)).getSingleResult()
+    
+    def findBaseId(self, tableName, baseIdColumnName, oldIdColumnName, oldId):
+        return (entityManager.createNativeQuery(('SELECT %s FROM %s WHERE %s = %d' % (baseIdColumnName, tableName, oldIdColumnName, oldId))).getSingleResult())[0]
+    
+    def communityInsert(self, oldCommunity):
+        community = Community()
+        self.setDataAndPersistCommunity(oldCommunity, community)
+        entityManager.createNativeQuery(('INSERT INTO sync_community(`erp_community_id`, `access_community_id`) VALUES(%d, %d)' % (community.getId(), oldCommunity.getId()))).executeUpdate()
+        
+    def setDataAndPersistCommunity(self, oldCommunity, community):
+        community.setName(oldCommunity.getNazwa())
+        community.setArea(float(oldCommunity.getPow()))
+        community.setInDate(oldCommunity.getDataprz())
+        community.setOutDate(oldCommunity.getDatawyl())
+        entityManager.persist(community)
+    
+    def syncDataExists(self, tableName, idColumnName, id):
+        try:
+            entityManager.createNativeQuery(('SELECT * FROM %s WHERE %s = %s' % (tableName, idColumnName, id))).getSingleResult()
+            return True
+        except:
+            return False
