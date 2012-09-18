dc843fdac937eb17495fc267fef1c741082ca719
Tomasz Kopacki <tomasz@kopacki.eu>
2012-09-18 12:23:11 +0200

    .

--- src/main/java/pl/reaper/container/beans/JythonAuthenticator.java
+++ src/main/java/pl/reaper/container/beans/JythonAuthenticator.java
@@ -3,13 +3,7 @@ package pl.reaper.container.beans;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.persistence.TypedQuery;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Predicate;
-import javax.persistence.criteria.Root;
-import pl.reaper.container.data.UserGroup;
-import pl.reaper.container.data.UserGroup_;
+import javax.persistence.Query;
 
 @Stateless
 public class JythonAuthenticator implements JythonAuthenticatorLocal {
@@ -19,14 +13,7 @@ public class JythonAuthenticator implements JythonAuthenticatorLocal {
 
     @Override
     public boolean isUserInRole(String userName, String groupName) {
-        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
-        CriteriaQuery query = criteriaBuilder.createQuery();
-        Root<UserGroup> root = query.from(UserGroup.class);
-        Predicate groupNameCriteria = criteriaBuilder.equal(root.get(UserGroup_.name), groupName);
-        Predicate userNameCriteria = criteriaBuilder.equal(root.get(UserGroup_.user), userName);
-        query.where(groupNameCriteria, userNameCriteria);
-        query.select(criteriaBuilder.count(root));
-        TypedQuery<Long> q = this.entityManager.createQuery(query);
-        return q.getSingleResult() == 1;
+        Query query = entityManager.createQuery(String.format("SELECT count(s) FROM UserGroup s WHERE s.user.login = %s and s.name = %s", userName, groupName));
+        return query.getSingleResult() == 1;
     }
 }
