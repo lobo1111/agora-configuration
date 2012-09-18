829578f754fa91cf88b893c77378e731e75b9fa7
Tomasz Kopacki <tomasz@kopacki.eu>
2012-09-18 14:56:07 +0200

    .

--- src/main/java/pl/reaper/container/beans/JythonAuthenticator.java
+++ src/main/java/pl/reaper/container/beans/JythonAuthenticator.java
@@ -14,7 +14,10 @@ public class JythonAuthenticator implements JythonAuthenticatorLocal {
     @Override
     public boolean isUserInRole(String userName, String groupName) {
         System.out.println("!!!!!!!!!" + groupName);
-        Query query = entityManager.createQuery(String.format("SELECT count(s) FROM UserGroup s WHERE s.user.login = '%s' and s.name = '%s'", userName, groupName));
+        System.out.println("!!!!!!!!!" + userName);
+        Query query = entityManager.createQuery("SELECT count(s) FROM UserGroup s WHERE s.user.login = :userName and s.name = :groupName")
+                .setParameter("userName", userName)
+                .setParameter("groupName", groupName);
         return query.getSingleResult() == 1;
     }
 }
