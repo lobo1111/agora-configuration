54c628b53c64c8902e1e94aba578eae614381302
Tomasz Kopacki <tomasz@kopacki.eu>
2012-09-19 12:21:22 +0200

    .

--- src/main/java/pl/reaper/container/beans/TemplateBeanLocal.java
+++ src/main/java/pl/reaper/container/beans/TemplateBeanLocal.java
@@ -1,15 +1,7 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package pl.reaper.container.beans;
 
 import javax.ejb.Local;
 
-/**
- *
- * @author tomek
- */
 @Local
 public interface TemplateBeanLocal {
 
