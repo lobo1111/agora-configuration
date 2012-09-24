232653637391b7611e240db34285af4935ab36b3
Tomasz Kopacki <tomasz@kopacki.eu>
2012-09-24 10:20:18 +0200

    .

--- src/main/java/pl/reaper/container/jython/VariableParser.java
+++ src/main/java/pl/reaper/container/jython/VariableParser.java
@@ -14,13 +14,15 @@ public class VariableParser {
         this.variables = variables;
     }
 
-    public String parse() {
+    public String parse() throws StringIndexOutOfBoundsException {
         StringBuilder result = new StringBuilder();
         Logger.getLogger(VariableParser.class.getName()).log(Level.FINE, "Parsing string: {0}", toParse);
         int previousEndMarker = 0;
         int from;
         int to;
         while ((from = toParse.indexOf("[:", previousEndMarker)) > -1 && (to = toParse.indexOf("]", previousEndMarker)) > -1) {
+            System.out.println("from" + from + ", to:" + to);
+            System.err.println("from" + from + ", to:" + to);
             Logger.getLogger(VariableParser.class.getName()).log(Level.FINE, "Found variable at {0}-{1}", new Object[]{from, to});
             String toReplace = toParse.substring(from + 2, to);
             Logger.getLogger(VariableParser.class.getName()).log(Level.FINE, "Replacing {0}", toReplace);
