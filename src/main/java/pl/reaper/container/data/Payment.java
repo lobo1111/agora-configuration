28489eab5ca9d3c130d2ed50bba18be3688ec426
Tomasz Kopacki <tomasz@kopacki.eu>
2012-10-22 11:53:14 +0200

    payments

---
+++ src/main/java/pl/reaper/container/data/Payment.java
@@ -0,0 +1,100 @@
+package pl.reaper.container.data;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@Entity
+@Table(name = "payment")
+@XmlRootElement
+public class Payment implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id")
+    private int id;
+    @JoinColumn(name = "possession_id", referencedColumnName = "id")
+    @ManyToOne(optional = false)
+    private Possession possession;
+    @JoinColumn(name = "type_id", referencedColumnName = "id")
+    @ManyToOne(optional = false)
+    private Dictionary type;
+    @JoinColumn(name = "status_id", referencedColumnName = "id")
+    @ManyToOne(optional = false)
+    private Dictionary status;
+    @Column(name = "income")
+    private BigDecimal income;
+    @Basic(optional = false)
+    @Size(min = 1, max = 255)
+    @Column(name = "description")
+    private String description;
+    @Column(name = "auto")
+    private boolean auto;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Possession getPossession() {
+        return possession;
+    }
+
+    public void setPossession(Possession possession) {
+        this.possession = possession;
+    }
+
+    public Dictionary getType() {
+        return type;
+    }
+
+    public void setType(Dictionary type) {
+        this.type = type;
+    }
+
+    public Dictionary getStatus() {
+        return status;
+    }
+
+    public void setStatus(Dictionary status) {
+        this.status = status;
+    }
+
+    public BigDecimal getIncome() {
+        return income;
+    }
+
+    public void setIncome(BigDecimal income) {
+        this.income = income;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isAuto() {
+        return auto;
+    }
+
+    public void setAuto(boolean auto) {
+        this.auto = auto;
+    }
+}
