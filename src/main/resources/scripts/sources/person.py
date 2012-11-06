dd0b38d9b3d2f4a0d75f3012c7c7c20ce826d5be
Tomasz Kopacki <tomasz@kopacki.eu>
2012-11-06 14:06:05 +0100

    owners

---
+++ src/main/resources/scripts/sources/person.py
@@ -0,0 +1,29 @@
+from pl.reaper.container.data import Person
+
+class PersonManager(Container):
+    def __init__(self):
+        pass
+    
+    def create(self):
+        person = Person()
+        person.setFirstName(vars.get('firstName'))
+        person.setLastName(vars.get('lastName'))
+        person.setNip(vars.get('nip'))
+        person.setPesel(vars.get('pesel'))
+        person.setEmail(vars.get('email'))
+        person.setPhoneNumber1(vars.get('phone1'))
+        person.setPhoneNumber2(vars.get('phone2'))
+        person.setPhoneNumber3(vars.get('phone3'))
+        person.setAddress(self.getAddress())
+        entityManager.persist(person)
+        
+    def getAddress(self):
+        address = Address()
+        address.setStreet(vars.get('street'))
+        address.setHouseNumber(vars.get('houseNumber'))
+        address.setFlatNumber(vars.get('flatNumber'))
+        address.setPostalCode(vars.get('postal'))
+        address.setCity(vars.get('city'))
+        entityManager.persist(address)
+        entityManager.flush()
+        return address
