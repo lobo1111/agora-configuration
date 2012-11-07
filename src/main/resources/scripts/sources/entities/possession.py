3b5923aafc836d89acf3a28926b16f57a962e80e
Tomasz Kopacki <tomasz@kopacki.eu>
2012-11-07 14:02:59 +0100

    owners

---
+++ src/main/resources/scripts/sources/entities/possession.py
@@ -0,0 +1,16 @@
+class PersonManager(Container):
+    _logger = Logger([:_scriptId])
+    
+    def addPersonOwner(self):
+        person = self.findPerson();
+        possession = self.findPossession()
+        possession.getPeople().add(person)
+        entityManager.persist(possession)
+        
+    def findPerson(self):
+        id = vars.get('owner')
+        return entityManager.createQuery('Select person From Person person Where person.id = ' + id).getSingleResult()
+    
+    def findPossession(self):
+        id = vars.get('possession')
+        return entityManager.createQuery('Select possession From Possession possession Where possession.id = ' + id).getSingleResult()
