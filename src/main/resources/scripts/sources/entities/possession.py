687c1559f85aaf3ddf270b05635eed53796c1ba6
Tomasz Kopacki <tomasz@kopacki.eu>
2012-11-07 14:32:51 +0100

    owners

--- src/main/resources/scripts/sources/entities/possession.py
+++ src/main/resources/scripts/sources/entities/possession.py
@@ -10,16 +10,36 @@ class PossessionManager(Container):
             entityManager.persist(possession)
         else:
             self._logger.info('Bound allready exists: [person:%d]<->[possession:%d]' % (person.getId(), possession.getId()));
+            
+    def addCompanyOwner(self):
+        company = self.findCompany();
+        possession = self.findPossession()
+        if not self.hasCompanyBound(company, possession):
+            self._logger.info('Creating new bound: [company:%d]<->[possession:%d]' % (company.getId(), possession.getId()));
+            possession.getCompanies().add(company)
+            entityManager.persist(possession)
+        else:
+            self._logger.info('Bound allready exists: [company:%d]<->[possession:%d]' % (company.getId(), possession.getId()));
         
-    def hasBound(self, toBound, possession):
+    def hasPeopleBound(self, toBound, possession):
         for person in possession.getPeople():
             if(toBound.getId() == person.getId()):
+                return True
+        return False
+    
+    def hasPeopleBound(self, toBound, possession):
+        for company in possession.getCompanies():
+            if(toBound.getId() == company.getId()):
                 return True
         return False
         
     def findPerson(self):
         id = vars.get('owner')
         return entityManager.createQuery('Select person From Person person Where person.id = ' + id).getSingleResult()
+    
+    def findCompany(self):
+        id = vars.get('owner')
+        return entityManager.createQuery('Select company From Company company Where company.id = ' + id).getSingleResult()
     
     def findPossession(self):
         id = vars.get('possession')
