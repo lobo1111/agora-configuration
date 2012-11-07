8cc5173379ad492f0763b570485249436ff8a2be
Tomasz Kopacki <tomasz@kopacki.eu>
2012-11-07 14:16:14 +0100

    owners

--- src/main/resources/scripts/sources/entities/possession.py
+++ src/main/resources/scripts/sources/entities/possession.py
@@ -4,8 +4,15 @@ class PossessionManager(Container):
     def addPersonOwner(self):
         person = self.findPerson();
         possession = self.findPossession()
-        possession.getPeople().add(person)
-        entityManager.persist(possession)
+        if not self.hasBound(person, possession):
+            possession.getPeople().add(person)
+            entityManager.persist(possession)
+        
+    def hasBound(self, toBound, possession):
+        for person in possession.getPeople():
+            if(toBound.getId() == person.getId()):
+                return true
+        return false
         
     def findPerson(self):
         id = vars.get('owner')
