678c34adfd2a77437aaf3e3718c853f0bfeaaab4
Tomasz Kopacki <tomasz@kopacki.eu>
2012-11-13 14:03:42 +0100

    zpk

--- src/main/java/pl/reaper/container/data/Zpk.java
+++ src/main/java/pl/reaper/container/data/Zpk.java
@@ -2,14 +2,20 @@ package pl.reaper.container.data;
 
 import javax.persistence.Basic;
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
 
+@Entity
+@Table(name = "zpk")
+@XmlRootElement
 public class Zpk {
 
     private static final long serialVersionUID = 1L;
