af00b9f3e7f162c3f6c5cc858dd600b2e6709207
Tomasz Kopacki <tomasz@kopacki.eu>
2013-02-26 14:16:50 +0100

    .

---
+++ src/main/java/pl/reaper/container/PaymentSchedulerLog.java
@@ -0,0 +1,5 @@
+package pl.reaper.container;
+
+public class PaymentSchedulerLog {
+
+}
---
+++ src/main/java/pl/reaper/container/data/PaymentScheduler.java
@@ -0,0 +1,81 @@
+package pl.reaper.container.data;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@Entity
+@Table(name = "payment_scheduler")
+@XmlRootElement
+public class PaymentScheduler {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id")
+    private int id;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 255)
+    @Column(name = "name")
+    private String name;
+    @Column(name = "active")
+    private boolean active;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 20)
+    @Column(name = "day")
+    private String day;
+    @JoinColumn(name = "community_id", referencedColumnName = "id")
+    @ManyToOne(optional = false)
+    private Community community;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public String getDay() {
+        return day;
+    }
+
+    public void setDay(String day) {
+        this.day = day;
+    }
+
+    public Community getCommunity() {
+        return community;
+    }
+
+    public void setCommunity(Community community) {
+        this.community = community;
+    }
+}
