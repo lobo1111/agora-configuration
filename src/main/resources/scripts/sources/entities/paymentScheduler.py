0f9e6c799df1f7dc58696a2406320420d710c970
Tomasz Kopacki <tomasz@kopacki.eu>
2013-03-02 20:37:21 +0100

    .

--- src/main/resources/scripts/sources/entities/paymentScheduler.py
+++ src/main/resources/scripts/sources/entities/paymentScheduler.py
@@ -1,5 +1,48 @@
+from pl.reaper.container.data import PaymentScheduler
+from pl.reaper.container.data import PaymentSchedulerTemplate
 
 class PaymentSchedulerManager(Container):
+    def __init__(self):
+        self._dictManager = DictionaryManager()
     
     def create(self):
-        pass
+        paymentScheduler = PaymentScheduler()
+        self.setData(paymentScheduler)
+        entityManager.persist(paymentScheduler)
+        
+    def setData(self, ps):
+        ps.setName(vars.get('paymentSchedulerName'))
+        if vars.get('paymentSchedulerActive') == 'true':
+            ps.setActive(True)
+        ps.setDay(vars.get('paymentSchedulerDay'))
+        ps.setCommunity(self.findCommunity(vars.get('communityId')))
+        self.setTemplateData(ps)
+        
+    def setTemplateData(self, ps):
+        data = PaymentSchedulerTemplate()
+        data.setAmount(vars.get('paymentAmount'))
+        data.setAccount(self.findAccount(vars.get('accountId')))
+        data.setDescription(vars.get('paymentDescription'))
+        data.setType(self.findType(vars.get('paymentType')))
+        if vars.get('paymentBook') == 'true':
+            data.setAutoBook(true)
+        data.setZpk(self.findZpk(vars.get('zpkId')))
+        data.setBookingPeriod(self.findBookingPeriod(vars.get('paymentBookingPeriod')))
+        ps.getPaymentSchedulerTemplates().clear()
+        ps.getPaymentSchedulerTemplates().add(data)
+    
+    def findCommunity(self, communityId):
+        return CommunityManager().findCommunityById(communityId)
+    
+    def findAccount(self, accountId):
+        return AccountManager().findAccountById(accountId)
+    
+    def findType(self, typeId):
+        self._dictManager.getDictionaryInstance(typeId)
+    
+    def findZpk(self, zpkId):
+        return ZpkManager().findZpkById(zpkId)
+    
+    def findBookingPeriod(self, periodId):
+        return BookingPeriodManager().findBookingPeriodById(periodId)
+    
