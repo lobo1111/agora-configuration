e0b7f8b5ebfd775fc573dd25a39302144ce89511
Tomasz Kopacki <tomasz@kopacki.eu>
2013-03-04 13:43:39 +0100

    .

--- src/main/resources/scripts/sources/entities/paymentScheduler.py
+++ src/main/resources/scripts/sources/entities/paymentScheduler.py
@@ -10,6 +10,11 @@ class PaymentSchedulerManager(Container):
         self.setData(paymentScheduler)
         entityManager.persist(paymentScheduler)
         
+    def update(self):
+        paymentScheduler = self.findPaymentSchedulerById(vars.get('id'))
+        self.setData(paymentScheduler)
+        entityManager.persist(paymentScheduler)
+        
     def setData(self, ps):
         ps.setName(vars.get('paymentSchedulerName'))
         if vars.get('paymentSchedulerActive') == 'true':
@@ -19,7 +24,7 @@ class PaymentSchedulerManager(Container):
         self.setTemplateData(ps)
         
     def setTemplateData(self, ps):
-        data = PaymentSchedulerTemplate()
+        data = self.getOrCreatePaymentSchedulerTemplate(ps)
         data.setPaymentScheduler(ps)
         data.setAmount(BigDecimal(vars.get('paymentAmount')))
         data.setDescription(vars.get('paymentDescription'))
@@ -39,4 +44,14 @@ class PaymentSchedulerManager(Container):
     
     def findZpk(self, zpkId):
         return ZpkManager().findZpkById(zpkId)
+    
+    def findPaymentSchedulerById(self, psId):
+        sql = "Select ps From PaymentScheduler ps Where ps.id = '%s'" % psId
+        return entityManager.createQuery(sql).getSingleResult()
+    
+    def getOrCreatePaymentSchedulerTemplate(self, entity):
+        if entity.getPaymentSchedulerTemplates().size() == 1:
+            return entity.getPaymentSchedulerTemplates().get(0)
+        else:
+            return PaymentSchedulerTemplate()
     
