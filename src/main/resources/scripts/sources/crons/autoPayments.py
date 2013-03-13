a05a6302e97322bffa4c3eeb3a844c5c3d4020da
Tomasz Kopacki <tomasz@kopacki.eu>
2013-03-13 15:06:31 +0100

    .

--- src/main/resources/scripts/sources/crons/autoPayments.py
+++ src/main/resources/scripts/sources/crons/autoPayments.py
@@ -10,7 +10,7 @@ class CronAutoPayment(Container):
         self._logger.info('Found %s documents ready for processing' % (str(self._documents.size())))
         for document in self._documents:
             self._logger.info('Processing document - id:%s' % document.getId())
-#            self.process(document)
+            self.process(document)
             self._logger.info('Document processed')
         self._logger.info('Cron auto payment finished.')
         
@@ -20,14 +20,14 @@ class CronAutoPayment(Container):
     
     def process(self, document):
         autoPayment = self.matchAutoPayment(document)
-        if autoPayment is None:
-            self._logger.info("Can't match this document to any account.")
-            self.setAsUnknown(document)
-        else:  
-            self._logger.info("Document matched, creating payments....")
-            self.createPayments(document)
-            self.setAsProcessed(document)
-        entityManager.persist(document)
+#        if autoPayment is None:
+#            self._logger.info("Can't match this document to any account.")
+#            self.setAsUnknown(document)
+#        else:  
+#            self._logger.info("Document matched, creating payments....")
+#            self.createPayments(document)
+#            self.setAsProcessed(document)
+#        entityManager.persist(document)
         
     def matchAutoPayment(self, document):
         try:
