bb61db6f9f85f57aa87649ecae55e432e72e51cf
Tomasz Kopacki <tomasz@kopacki.eu>
2013-03-19 11:18:32 +0100

    .

---
+++ src/main/java/pl/reaper/container/data/Obligation.java
@@ -0,0 +1,101 @@
+package pl.reaper.container.data;
+
+import java.io.Serializable;
+import java.util.Collection;
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@Entity
+@Table(name = "obligation")
+@XmlRootElement
+public class Obligation implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id")
+    private Integer id;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 255)
+    @Column(name = "name")
+    private String name;
+    @JoinColumn(name = "community_id", referencedColumnName = "id")
+    @ManyToOne(optional = false)
+    private Community community;
+    @JoinColumn(name = "zpk_id", referencedColumnName = "id")
+    @ManyToOne(cascade = CascadeType.PERSIST, optional = false)
+    private ZakladowyPlanKont zpk;
+    @OneToMany(mappedBy = "obligation")
+    private Collection<ObligationLog> obligationLog;
+    @ManyToMany
+    @JoinTable(
+        name = "obligation_possession_zpk", joinColumns =
+    @JoinColumn(name = "possession_zpk_id", referencedColumnName = "id"),
+    inverseJoinColumns =
+    @JoinColumn(name = "obligation_id", referencedColumnName = "id"))
+    private Collection<ZakladowyPlanKont> zpks;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Community getCommunity() {
+        return community;
+    }
+
+    public void setCommunity(Community community) {
+        this.community = community;
+    }
+
+    public ZakladowyPlanKont getZpk() {
+        return zpk;
+    }
+
+    public void setZpk(ZakladowyPlanKont zpk) {
+        this.zpk = zpk;
+    }
+
+    public Collection<ObligationLog> getObligationLog() {
+        return obligationLog;
+    }
+
+    public void setObligationLog(Collection<ObligationLog> obligationLog) {
+        this.obligationLog = obligationLog;
+    }
+
+    public Collection<ZakladowyPlanKont> getZpks() {
+        return zpks;
+    }
+
+    public void setZpks(Collection<ZakladowyPlanKont> zpks) {
+        this.zpks = zpks;
+    }
+}
---
+++ src/main/java/pl/reaper/container/data/ObligationLog.java
@@ -0,0 +1,59 @@
+package pl.reaper.container.data;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@Entity
+@Table(name = "obligation_log")
+@XmlRootElement
+public class ObligationLog implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id")
+    private Integer id;
+    @JoinColumn(name = "obligation_id", referencedColumnName = "id")
+    @ManyToOne(optional = false)
+    private Community obligation;
+    @Column(name = "timestamp")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date timestamp;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Community getObligation() {
+        return obligation;
+    }
+
+    public void setObligation(Community obligation) {
+        this.obligation = obligation;
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Date timestamp) {
+        this.timestamp = timestamp;
+    }
+}
