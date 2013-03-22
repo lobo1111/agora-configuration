e879cd1dd6b4a8fe693ebf9afffd3ab31c9d8b63
Tomasz Kopacki <tomasz@kopacki.eu>
2013-03-22 14:55:10 +0100

    .

---
+++ src/main/resources/scripts/sources/entities/obligationGroup.py
@@ -0,0 +1,29 @@
+from pl.reaper.container.data import ObligationGroup
+
+class ObligationGroupManager(Container):
+    
+     def create(self):
+        obligation = ObligationGroup()
+        self.setData(obligation)
+        entityManager.persist(obligation)
+        
+    def update(self):
+        obligation = self.findObligationGroupById(vars.get('id'))
+        self.setData(obligation)
+        entityManager.persist(obligation)
+        
+    def setData(self, obligation):
+        obligation.setName(vars.get('obligationGroupName'))
+        obligation.setCommunity(self.findCommunity(vars.get('obligationGroupCommunityId')))
+        
+    def findObligationGroupById(self, id):
+        return entityManager.createQuery('Select o From ObligationGroup o Where o.id = ' + str(id)).getSingleResult()
+    
+    def findContractor(self, id):
+        return CompanyManager().findCompanyById(id)
+    
+    def findContractorZpk(self, id):
+        return ZpkManager().findZpkById(id)
+    
+    def findCommunity(self, id):
+        return CommunityManager().findCommunityById(id)
