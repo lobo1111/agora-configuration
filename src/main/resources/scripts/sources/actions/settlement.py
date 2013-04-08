437e69352f8a9ed739aa6ec6f9dab0528a9e312c
Tomasz Kopacki <tomasz@kopacki.eu>
2013-04-08 16:01:26 +0200

    script engine rebuild

---
+++ src/main/resources/scripts/sources/actions/settlement.py
@@ -0,0 +1,32 @@
+class SettleManager(Container):
+    _logger = Logger([:_scriptId])
+    
+    def settle(self):
+        groupManager = self.findObligationGroup()
+        balance = self.calculateBalance(groupManager)
+        for zpk in groupManager.getZpks():
+            amount = (zpk.getPossession().getShare().floatValue / 100.0) * balance
+            self.createPayment(zpk, amount)
+            
+    def calculateBalance(self, groupManager):
+        income = self.calculateIncome(groupManager.getZpks())
+        expenditure = self.calculateExpenditure(groupManager.getObligations)
+        return income - expenditure
+    
+    def calculateIncome(self, zpks):
+        income = 0.0
+        for zpk in zpks:
+            income += zpk.getDebit().floatValue()
+        return income
+    
+    def calculateExpenditure(self, obligations):
+        expenditure = 0.0
+        for obligation in obligations:
+            expenditure += obligation.getZpk().getDebit().floatValue()
+        return expenditure
+    
+    def createPayment(self, zpk, amount):
+        self._logger.info("Creating expenditure(%f) for %s" % (amount, zpk.getNumber()))
+        
+    def findObligationGroup(self):
+        return ObligationGroupManager().findObligationGroupById(vars.get('obligationGroupId'))
