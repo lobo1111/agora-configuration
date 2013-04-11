f11bea1f516f6eb4ba677b64f8e466236b0799b1
Tomasz Kopacki <tomasz@kopacki.eu>
2013-04-11 11:26:05 +0200

    .

--- src/main/java/pl/reaper/container/jython/Pool.java
+++ src/main/java/pl/reaper/container/jython/Pool.java
@@ -10,12 +10,15 @@ public class Pool {
 
     private List<ScriptEngineWrapper> pool = new ArrayList<>();
     private List<ScriptEngineWrapper> working = new ArrayList<>();
+    private final String scriptName;
 
     public Pool(String scriptName) {
+        this.scriptName = scriptName;
         Logger.getLogger(JythonBean.class.getName()).log(Level.SEVERE, "Pool for " + scriptName + " created.");
     }
 
     public boolean isEmpty() {
+        Logger.getLogger(JythonBean.class.getName()).log(Level.SEVERE, "Pool(" + scriptName + ") is empty");
         return pool.isEmpty();
     }
 
@@ -23,18 +26,22 @@ public class Pool {
         if (!pool.isEmpty()) {
             ScriptEngineWrapper engine = pool.remove(0);
             working.add(engine);
+            Logger.getLogger(JythonBean.class.getName()).log(Level.SEVERE, "Pool(" + scriptName + ") - engine reserved(" + engine + ")");
             return engine;
         } else {
+            Logger.getLogger(JythonBean.class.getName()).log(Level.SEVERE, "Pool(" + scriptName + ") - can't get engine from empty pool !");
             throw new ScriptEnginePoolIsEmptyException();
         }
     }
 
     public void release(ScriptEngineWrapper engine) throws UnknownScriptEngineException {
+        Logger.getLogger(JythonBean.class.getName()).log(Level.SEVERE, "Pool(" + scriptName + ") - engine released(" + engine + ")");
         working.remove(engine);
         pool.add(engine);
     }
 
     public void put(ScriptEngineWrapper engine) {
+        Logger.getLogger(JythonBean.class.getName()).log(Level.SEVERE, "Pool(" + scriptName + ") - engine created(" + engine + ")");
         pool.add(engine);
     }
 }
