5dd5bedd9a47f3a108e9555ea5e6bea5c9fbf3c6
lobo <lobo@lobo.dom.local>
2013-05-20 21:53:03 +0200

    .

---
+++ src/main/java/pl/reaper/container/data/helpers/SimpleLongResponse.java
@@ -0,0 +1,18 @@
+package pl.reaper.container.data.helpers;
+
+public class SimpleLongResponse {
+
+    private long value;
+
+    public SimpleLongResponse(long value) {
+        this.value = value;
+    }
+
+    public long getValue() {
+        return value;
+    }
+
+    public void setValue(long value) {
+        this.value = value;
+    }
+}
