4077cb51e59d380e1d40ba4699d77b707d6c315e
Tomasz Kopacki <tomasz@kopacki.eu>
2013-05-20 15:37:05 +0200

    .

---
+++ src/main/java/pl/reaper/container/data/helpers/SimpleBooleanResponse.java
@@ -0,0 +1,14 @@
+package pl.reaper.container.data.helpers;
+
+public class SimpleBooleanResponse {
+
+    private boolean value;
+
+    public boolean isValue() {
+        return value;
+    }
+
+    public void setValue(boolean value) {
+        this.value = value;
+    }
+}
