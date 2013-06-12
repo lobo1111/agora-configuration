b98d951d15ebef1e06bb820e4bcaaa4cec1e02a1
Tomasz Kopacki <tomasz@kopacki.eu>
2013-06-12 15:11:43 +0200

    .

--- src/main/resources/scripts/sources/entities/report.py
+++ src/main/resources/scripts/sources/entities/report.py
@@ -13,18 +13,22 @@ class ReportManager(Container):
 
     def generateXML(self, report):
         self._logger.info('Generating report %s...' % report.getName())
-        xml = '<?xml version="1.0" encoding="UTF-8" standalone="yes" ?>'
-        xml += '<report>'
+        xml = ''
+        xml += '<html>'
         for section in report.getSections():
             xml += self.generateSectionXml(report, section)
-        xml += '</report>'
+        xml += '</html>'
         self._logger.info('Report generated')
         return xml
 
     def generateSectionXml(self, report, section):
         self._logger.info('Generating section...')
-        xml = '<section>'
-        data = self.getData(section.getQuery(), report.getFilters())
+        xml = '<table>'
+        xml += '<th>'
+        for attribute in report.getAttributes():
+            xml += '<td>' + attribute.getAttributeAlias() + '</td>'
+        xml += '</th>'
+        data = self.getData(section.getQuery())
         for row in data:
             self._logger.info('Generating section row...')
             xml += self.generateRowXml(report, row)
@@ -33,29 +37,26 @@ class ReportManager(Container):
             self._logger.info('Generating section children...')
             xml += self.generateSectionXml(report, child)
             self._logger.info('Section children generated...')
-        xml += '</section>'
+        xml += '</table>'
         self._logger.info('Section generated')
         return xml
 
     def generateRowXml(self, report, row):
-        xml = ''
+        xml = '<tr>'
         for attribute in report.getAttributes():
             self._logger.info('Adding section attribute[%s=%s]' % (attribute.getAttribute(), attribute.getAttributeAlias()))
-            xml += '<attribute>'
-            xml += '<name>'
-            xml += attribute.getAttributeAlias()
-            xml += '</name>'
-            xml += '<value>'
+            xml += '<td>'
             if row.containsKey(attribute.getAttribute()):
                 xml += str(row.get(attribute.getAttribute()))
             else:
                 self._logger.warn('Attribute not available - %s' % attribute.getAttribute())
                 xml += 'ATTRIBUTE NOT AVAILABLE - %s' % attribute.getAttribute()
-            xml += '</value>'
-            xml += '</attribute>'
+            xml += '</td>'
+        xml += '</tr>'
         return xml
 
-    def getData(self, query, filters):
+    def getData(self, query):
+        query = query.replace('{:where}', vars.get('where'))
         queryInstance = entityManager.createQuery(query);
         queryInstance.setHint(QueryHints.RESULT_TYPE, ResultType.Map);
         return queryInstance.getResultList()
