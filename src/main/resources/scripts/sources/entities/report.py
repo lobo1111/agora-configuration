67e12499d07a504e426cceaa14e0c3f39bb179c8
lobo <lobo@lobo.dom.local>
2013-06-15 23:36:52 +0200

    .

--- src/main/resources/scripts/sources/entities/report.py
+++ src/main/resources/scripts/sources/entities/report.py
@@ -89,7 +89,7 @@ class ReportManager(Container):
         xml += report.getName()
         xml += self._html.closeDiv()
         xml += self._html.openDiv('float: none;')
-        xml += 'Bilans na dzień: ' + datetime.datetime.now().day + '-' + datetime.datetime.now().month + '-' + datetime.datetime.now().year
+        xml += 'Bilans na dzień: %d-%d-%d' % (datetime.datetime.now().day, datetime.datetime.now().month, datetime.datetime.now().year)
         xml += self._html.closeDiv()
         xml += self._html.closeDiv()
         return xml
