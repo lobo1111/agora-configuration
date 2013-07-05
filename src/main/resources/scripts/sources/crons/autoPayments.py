e835b5c7060774fcf6f837f0c317d459371e9089
Tomasz Kopacki <tomasz@kopacki.eu>
2013-07-05 14:03:13 +0200

    .

--- src/main/resources/scripts/sources/crons/autoPayments.py
+++ src/main/resources/scripts/sources/crons/autoPayments.py
@@ -92,7 +92,7 @@ class CronAutoPayment(Container):
         vars.put('zpkId', zpk.getId())
         vars.put('paymentBookingPeriod', period.getId())
         vars.put('paymentDescription', position.getTitle())
-        vars.put('communityId', str(zpk.getCommunity().getId()))
+        vars.put('communityId', zpk.getCommunity().getId())
         payment = PaymentManager().create()
         position.getPayments().add(payment)
 
