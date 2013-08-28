d9033f0cf94f8a89fb26ae37fe87146499e6df93
Tomasz Kopacki <tomasz@kopacki.eu>
2013-08-28 13:05:07 +0200

    - naliczenia i skladniki

---
+++ src/main/resources/scripts/sources/entities/element.py
@@ -0,0 +1,36 @@
+from pl.reaper.container.data import Element
+
+class ElementManager(Container):
+    _logger = Logger([:_scriptId])
+    _prefix = ''
+    
+    def setPrefix(self, prefix):
+        self._prefix = prefix
+    
+    def create(self):
+        element = Element()
+        self.setElementData(element)
+        self.saveElement(element)
+        return element
+        
+    def update(self):
+        element = self.findElementById(vars.get(self._prefix + 'id'))
+        self.setElementData(element)
+        self.saveElement(element)
+        return element
+        
+    def setElementData(self, element):
+        element.setName(vars.get(self._prefix + 'name'))
+        element.setKey(vars.get(self._prefix + 'key'))
+        element.setGlobalValue(vars.get(self._prefix + 'globalValue'))
+        element.setGroup(self.findGroupByKey(vars.get(self._prefix + 'group')))
+        
+    def saveElement(self, element):
+        entityManager.persist(element)
+        entityManager.flush()
+        
+    def findElementById(self, id):
+        return entityManager.createQuery('Select element From Element element Where element.id = ' + id).getSingleResult()
+    
+    def findGroupByKey(self, key):
+        return entityManager.createQuery('Select dict From Dictionary dict Where dict.key = ' + key).getSingleResult()
