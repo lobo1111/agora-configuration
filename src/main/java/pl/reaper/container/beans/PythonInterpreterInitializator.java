af45d2eb3737e67ddac7f7a08d46e9f8528c934f
Tomasz Kopacki <tomasz@kopacki.eu>
2013-09-20 14:00:05 +0200

    - naliczenia i skladniki

---
+++ src/main/java/pl/reaper/container/beans/PythonInterpreterInitializator.java
@@ -0,0 +1,20 @@
+package pl.reaper.container.beans;
+
+import java.util.Properties;
+import javax.ejb.LocalBean;
+import javax.ejb.Startup;
+import javax.ejb.Stateless;
+import org.python.util.PythonInterpreter;
+
+@Stateless
+@LocalBean
+@Startup
+public class PythonInterpreterInitializator {
+
+    public void init() {
+        Properties properties = new Properties();
+        properties.setProperty("python.path", "/usr/share/jython/Lib");
+        PythonInterpreter.initialize(System.getProperties(), properties, new String[]{});
+        System.out.println("PythonInterpreter initialized.");
+    }
+}
