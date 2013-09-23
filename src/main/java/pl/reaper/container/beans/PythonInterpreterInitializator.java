0546c419336c873a378574277a928fa41efba1d6
Tomasz Kopacki <tomasz@kopacki.eu>
2013-09-23 11:55:22 +0200

    - naliczenia i skladniki

--- src/main/java/pl/reaper/container/beans/PythonInterpreterInitializator.java
+++ src/main/java/pl/reaper/container/beans/PythonInterpreterInitializator.java
@@ -5,23 +5,24 @@ import java.util.Properties;
 import javax.annotation.PostConstruct;
 import javax.ejb.Singleton;
 import javax.ejb.Startup;
+import org.python.core.PySystemState;
 import org.python.util.PythonInterpreter;
 
 @Startup
 @Singleton
 public class PythonInterpreterInitializator {
-    
+
     private String CACHE_DIR = "/opt/container/cache/";
     private File cacheDir = new File(CACHE_DIR);
 
     @PostConstruct
     public void init() {
         Properties properties = new Properties();
-//        properties.setProperty("python.path", "/usr/share/jython/Lib");
-        properties.setProperty("python.path", "/opt/glassfish/domains/devel/applications/Container-0.1");
+        properties.setProperty("python.path", "/usr/share/jython/Lib");
         PythonInterpreter.initialize(System.getProperties(), properties, new String[]{});
-        for(File file: cacheDir.listFiles()) {
-            if(file.delete()) {
+        PySystemState.add_classdir("/opt/glassfish/domains/devel/applications/Container-0.1");
+        for (File file : cacheDir.listFiles()) {
+            if (file.delete()) {
                 System.out.println("Cached file " + file.getName() + " deleted.");
             } else {
                 System.out.println("Cached file " + file.getName() + " can't be deleted.");
