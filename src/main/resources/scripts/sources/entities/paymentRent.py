3b93a56a316af01b3fd8da08d2cde9d0695792ed
Tomasz Kopacki <tomasz@kopacki.eu>
2013-10-02 11:58:46 +0200

    - wplaty do naliczen - automatyczne dodawanie wplat

---
+++ src/main/resources/scripts/sources/entities/paymentRent.py
@@ -0,0 +1,41 @@
+from pl.reaper.container.data import PaymentRent
+from pl.reaper.container.data import PaymentRentDetails
+
+class PaymentRentManager(Container):
+    
+    def create(self):
+        paymentRent = PaymentRent()
+        self.setPaymentRentData(paymentRent)
+        self.savePaymentRent(paymentRent)
+        
+    def setPaymentRentData(self, paymentRent):
+        paymentRentDetails = PaymentRentDetails()
+        paymentRent.setPaymentRentDetails(paymentRentDetails)
+        paymentRent.setPossession(self.getPossession())
+        paymentRent.setMonth(self.getCurrentMonth())
+        paymentRent.setBookingPeriod(self.getBookingDate())
+        paymentRentDetails.setTile(vars.get('title'))
+        paymentRentDetails.setBookingDate(self.parseDate(vars.get('requestDate')))
+        paymentRentDetails.setRequestDate(self.parseDate(vars.get('bookingDate')))
+        paymentRentDetails.setClientName(vars.get('clientName'))
+        paymentRentDetails.setAccount(self.getAccount())
+        paymentRentDetails.setValue(float(vars.get('value')))
+    
+    def savePaymentRent(self, paymentRent):
+        entityManager.persist(paymentRent)
+        entityManager.flush()
+
+    def getPossession(self):
+        possessionId = int(vars.get('possessionId'))
+        return entityManager.createQuery('Select p From Possession p Where p.id = %d' % possessionId).getSingleResult()
+    
+    def getAccount(self):
+        accountId = int(vars.get('accountId'))
+        return entityManager.createQuery('Select a From Account a Where a.id = %d' % accountId).getSingleResult()
+    
+    def getCurrentMonth(self):
+        return entityManager.createQuery('SELECT dict.value FROM Dictionary dict JOIN dict.type dtype WHERE dtype.type = "PERIODS" AND dict.key = "CURRENT"').getSingleResult()
+    
+    def getBookingPeriod(self):
+        return entityManager.createQuery('Select period From BookingPeriod period Where period.defaultPeriod = true').getSingleResult()
+    
