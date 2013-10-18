4505f94da9b98197f9b609ca17b3d9c819db39ce
Tomasz Kopacki <tomasz@kopacki.eu>
2013-10-18 13:19:40 +0200

    - zpk - dokumenty platnosci, faktury itp

--- src/main/resources/scripts/sources/entities/zpk.py
+++ src/main/resources/scripts/sources/entities/zpk.py
@@ -50,7 +50,7 @@ class ZpkManager(Container):
         for zpk in community.getZpks():
             if(zpk.getType().getId() == dict.getId()):
                 zpks.append(int(zpk.getNumber()))
-        self._logger.info('Numbers already taken in the pool')
+        self._logger.info('Numbers already taken in the pool ' + zpks)
         for i in range(1, 1000):
             if not i in zpks:
                 self._logger.info('Number %d looks free' % i)
