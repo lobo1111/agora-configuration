059fab47c3ec0ff12e090b258232a6fef2112f5f
Tomasz Kopacki <tomasz@kopacki.eu>
2014-04-07 22:28:17 +0200

    .

--- src/main/resources/scripts/sources/update-community.py
+++ src/main/resources/scripts/sources/update-community.py
@@ -1,8 +1,13 @@
+import helpers
+helpers.entityManager = globals()['entityManager']
+helpers.svars = globals()['svars']
+helpers.properties = globals()['properties']
+
 from entities.Community import CommunityManager
 
 global svars, entityManager, properties
 manager = CommunityManager()
-manager.setSvars(svars)
-manager.setEntityManager(entityManager)
-manager.setProperties(properties)
+#manager.setSvars(svars)
+#manager.setEntityManager(entityManager)
+#manager.setProperties(properties)
 manager.update()
