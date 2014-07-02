aec1d9978ac53a4e79c037647b783b8ebab67a7f
Tomasz Kopacki <tomasz@kopacki.eu>
2014-07-02 13:24:16 +0200

    ...

--- src/main/resources/scripts/sources/create-zpks-dict.py
+++ src/main/resources/scripts/sources/create-zpks-dict.py
@@ -3,6 +3,6 @@ helpers.entityManager = globals()['entityManager']
 helpers.svars = globals()['svars']
 helpers.properties = globals()['properties']
 
-from entities.ZpkDitctionary import ZpkDictionaryManager
+from entities.ZpkDictionary import ZpkDictionaryManager
 manager = ZpkDictionaryManager()
 manager.create()
