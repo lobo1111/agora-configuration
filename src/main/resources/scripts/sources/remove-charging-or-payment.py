1a2b846661a8b1e0e1faa1b4badbadda95c8b4b8
Tomasz Kopacki <tomasz@kopacki.eu>
2015-01-26 21:44:46 +0100

    ...

--- src/main/resources/scripts/sources/remove-charging-or-payment.py
+++ src/main/resources/scripts/sources/remove-charging-or-payment.py
@@ -3,8 +3,9 @@ helpers.entityManager = globals()['entityManager']
 helpers.svars = globals()['svars']
 helpers.properties = globals()['properties']
 
-if int(globals()['svars'].get('id')) < 0:
-    id = int(globals()['svars'].get('id') * -1)
+id = int(globals()['svars'].get('id'))
+if id < 0:
+    id = id * -1
     globals()['svars'].put('id', str(id))
     from entities.PaymentRent import PaymentRentManager
     manager = PaymentRentManager()
