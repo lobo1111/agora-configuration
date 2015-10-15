994e5e2c4b196f3c6dce6a7c4dd68e6417d5a6c5
tomek <tomek@ADMINIB-2F10OB2>
2015-10-15 12:59:00 +0200

    .

---
+++ src/main/resources/scripts/sources/entities/BankCreditManager.py
@@ -0,0 +1,19 @@
+from pl.reaper.container.data import BankCredit
+from base.Container import Container
+
+class BankCreditManager(Container):
+
+    def create(self):
+        credit = BankCredit()
+        credit.setCreatedAt(self.parseDate(self._svars.get("createdAt")))
+        credit.setAmount(float(self._svars.get("amount")))
+        credt.setCommunity(self.getById("Community", self._svars.get("communityId")))
+        credt.setContractor(self.getById("Contractor", self._svars.get("contratorId")))
+        self.saveEntity(credit)
+
+    def remove(self):
+        pass
+        #note = self.findById("BankNote", self._svars.get('id'))
+        #if note.getInternalPayment() == None:
+        #    self._entityManager.remove(note)
+    
