afcbba762192661413202b4a1ee1d4940d9799b9
tomek <tomek@ADMINIB-2F10OB2>
2015-10-15 13:29:51 +0200

    .

--- src/main/resources/scripts/sources/entities/BankCredit.py
+++ src/main/resources/scripts/sources/entities/BankCredit.py
@@ -7,8 +7,8 @@ class BankCreditManager(Container):
         credit = BankCredit()
         credit.setCreatedAt(self.parseDate(self._svars.get("createdAt")))
         credit.setAmount(float(self._svars.get("amount")))
-        credit.setCommunity(self.getById("Community", self._svars.get("communityId")))
-        credit.setContractor(self.getById("Contractor", self._svars.get("contratorId")))
+        credit.setCommunity(self.findById("Community", self._svars.get("communityId")))
+        credit.setContractor(self.findById("Contractor", self._svars.get("contratorId")))
         self.saveEntity(credit)
 
     def remove(self):
