e551957b774057cf21392ddd4bb3208820cf4f7d
tomek <tomek@ADMINIB-2F10OB2>
2015-10-29 12:56:09 +0100

    .

--- src/main/resources/scripts/sources/base/Container.py
+++ src/main/resources/scripts/sources/base/Container.py
@@ -17,10 +17,13 @@ class Container:
             self._logger.info('Searching for %s/%s with value %s' % (entityName, field, value))
             sql = 'Select entity From %s entity where entity.%s = %s' % (entityName, field, value)
             self._logger.info(sql)
-            return self._entityManager.createQuery(sql).getSingleResult()
+            result = self._entityManager.createQuery(sql).getSingleResult()
+            self._logger.info('Entity found: %s' % str(result))
+            return result
         except:
             self._logger.info('Entity not found %s/%s with value %s' % (entityName, field, value))
             self._logger.info("Error message: %s" % sys.exc_info()[0])
+            self._logger.info("Error message: %s" % sys.exc_info()[1])
             return None
 
     def findById(self, entityName, id):
