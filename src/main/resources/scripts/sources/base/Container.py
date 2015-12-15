da578d061a19a1bb4fdd29338ce10ed5740d22ab
tomek <tomek@ADMINIB-2F10OB2>
2015-12-15 14:12:03 +0100

    .

--- src/main/resources/scripts/sources/base/Container.py
+++ src/main/resources/scripts/sources/base/Container.py
@@ -8,16 +8,21 @@ class Container:
     _logger = Logger()
     
     def startTransaction(self):
-        self._transaction = self._entityManager.beginTransaction()
+        try:
+            self._transaction = self._entityManager.beginTransaction()
+        except:
+            self._logger.info('Can\'t start transaction due to:')
+            self._logger.info("Error message: %s" % sys.exc_info()[0])
+            self._logger.info("Error message: %s" % sys.exc_info()[1])
         
     def commitTransaction(self):
         self._transaction.commit()
         
     def cancelTransaction(self):
-        self._transaction.rollback()
         self._logger.info('Transaction rolled back due to:')
         self._logger.info("Error message: %s" % sys.exc_info()[0])
         self._logger.info("Error message: %s" % sys.exc_info()[1])
+        self._transaction.rollback()
     
     def __init__(self):
         self._svars = helpers.svars
