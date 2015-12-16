a291e98d051e3a81ea5b28fb589d0b26cde3b356
tomek <tomek@ADMINIB-2F10OB2>
2015-12-16 11:19:51 +0100

    .

--- src/main/resources/scripts/sources/template-parser.py
+++ src/main/resources/scripts/sources/template-parser.py
@@ -1,6 +1,6 @@
 import helpers
 helpers.init(globals())
 
-#from base.TemplateParser import TemplateParser
-#manager = TemplateParser()
-#helpers.svars.put('output', manager.parse())
+from base.TemplateParser import TemplateParser
+manager = TemplateParser()
+helpers.svars.put('output', manager.parse())
