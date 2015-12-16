e13602ae2cf36041e23971f72a421fe11d692e14
tomek <tomek@ADMINIB-2F10OB2>
2015-12-16 16:02:05 +0100

    .

--- src/main/resources/scripts/sources/documents/Invoice.py
+++ src/main/resources/scripts/sources/documents/Invoice.py
@@ -72,7 +72,7 @@ class InvoiceManager(DocumentManager):
                 position.setDescription(self._svars.get(str(i) + '_positions_positionDescription'))
                 position.setCreditZpk(self.findZpk(invoice.getContractor().getZpks(), 'CONTRACTOR'))
                 position.setDebitZpk(self.findZpk(invoice.getContractor().getZpks(), 'CONTRACTOR_COST'))
-                self.bound(document, invoice)
+                self.bound(invoice, position)
     
     def updatePayments(self, invoice):
         for i in range(int(self._svars.get('paymentsCount'))):
@@ -88,7 +88,7 @@ class InvoiceManager(DocumentManager):
             payment = self.findOrCreatePayment(invoice, paymentId, prefix)
             payment.setCreditZpk(self.findZpk(invoice.getCommunity().getZpks(), 'RENT'))
             payment.setDebitZpk(self.findZpk(invoice.getContractor().getZpks(), 'CONTRACTOR'))
-            self.bound(invoice, position)
+            self.bound(invoice, payment)
     
     def findOrCreatePosition(self, invoice, positionId, prefix):
         if positionId == 0:
