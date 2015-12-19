a546294c9a785215f816caa5bf58154323ffd3da
tomek <tomek@ADMINIB-2F10OB2>
2015-12-19 22:18:13 +0100

    .

--- src/main/resources/scripts/sources/documents/migration/Invoice.py
+++ src/main/resources/scripts/sources/documents/migration/Invoice.py
@@ -51,6 +51,6 @@ class InvoiceMigrator(Container):
             documentPosition.setDocument(document)
     
     def collect(self):
-        sql = "Select i From Invoice i Where i.id Not In (Select attr.value From Document d Join d.attributes attr Where d.type = 'INVOICE' And attr.name = 'MIGRATED')"
+        sql = "Select i From Invoice i Where i.id Not In (Select FUNC('TO_NUMBER', attr.value) From Document d Join d.attributes attr Where d.type = 'INVOICE' And attr.name = 'MIGRATED')"
         return self._entityManager.createQuery(sql).getResultList()
     
