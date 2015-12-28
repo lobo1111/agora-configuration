632a3af4001dafc79a5769951e6917765e21b6bf
tomek <tomek@ADMINIB-2F10OB2>
2015-12-28 14:12:34 +0100

    .

--- src/main/resources/scripts/sources/create-bank-credit.py
+++ src/main/resources/scripts/sources/create-bank-credit.py
@@ -1,6 +1,6 @@
 import helpers
 helpers.init(globals())
 
-from entities.BankCredit import BankCreditManager
+from documents.BankCredit import BankCreditManager
 bankCreditManager = BankCreditManager()
 bankCreditManager.create()
--- src/main/resources/scripts/sources/documents/BankCredit.py
+++ src/main/resources/scripts/sources/documents/BankCredit.py
@@ -5,7 +5,6 @@ class BankCreditManager(DocumentManager):
     
     def create(self):
         credit = self.initDocument(self._type)
-        credit.setContractor(self.findById("Contractor", self._svars.get("contractorId")))
         positionCost = self.initPosition(credit)
         positionCost.setType("BANK_CREDIT_COST")
         positionCost.setZpkCredit(self.findZpk(credit.getContractor().getZpks(), 'CONTRACTOR'))
@@ -14,7 +13,10 @@ class BankCreditManager(DocumentManager):
             type = 'RENT'
         else:
             type = 'REPAIR_FUND'
-        positionCost.setZpkDebit(self.findZpk(credit.getCommunity().getZpks(), type))
+        credit.putAttribute("CHARGE_TYPE", type)
+        positionCost.setZpkDebit(self.findZpk(credit.getContractor().getZpks(), "CONTRACTOR_COST"))
+        positionCost.setZpkCredit(self.findZpk(credit.getCommunity().getZpks(), type))
+        self.bound(credit, positionCost)
         self.updatePayments(credit)
         return self.saveDocument(credit)
         
@@ -38,6 +40,9 @@ class BankCreditManager(DocumentManager):
             if id == 0 and not remove:
                 position = self.initPosition(credit, '_payments_')
                 position.setType("BANK_CREDIT_PAYMENT")
+                position.setZpkCredit(self.findZpk(credit.getContractor().getZpks(), "CONTRACTOR"))
+                position.setZpkDebit(self.findZpk(credit.getCommunity().getZpks(), credit.getAttribute('CHARGE_TYPE')))
+                self.bound(credit, position)
             if id != 0 and remove:
                 position = self.findById("DocumentPosition", id);
                 self.cancelPosition(position)
