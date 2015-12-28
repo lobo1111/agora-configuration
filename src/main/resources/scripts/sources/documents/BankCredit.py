8c3eb9501d894a8b1368fa0444982c9690332f29
tomek <tomek@ADMINIB-2F10OB2>
2015-12-28 14:18:42 +0100

    .

--- src/main/resources/scripts/sources/documents/BankCredit.py
+++ src/main/resources/scripts/sources/documents/BankCredit.py
@@ -7,15 +7,14 @@ class BankCreditManager(DocumentManager):
         credit = self.initDocument(self._type)
         positionCost = self.initPosition(credit)
         positionCost.setType("BANK_CREDIT_COST")
-        positionCost.setZpkCredit(self.findZpk(credit.getContractor().getZpks(), 'CONTRACTOR'))
         chargeDefaultAccount = self._svars.get("defaultAccount") == 'true'
         if chargeDefaultAccount:
             type = 'RENT'
         else:
             type = 'REPAIR_FUND'
         credit.putAttribute("CHARGE_TYPE", type)
-        positionCost.setZpkDebit(self.findZpk(credit.getContractor().getZpks(), "CONTRACTOR_COST"))
-        positionCost.setZpkCredit(self.findZpk(credit.getCommunity().getZpks(), type))
+        positionCost.setDebitZpk(self.findZpk(credit.getContractor().getZpks(), "CONTRACTOR_COST"))
+        positionCost.setCreditZpk(self.findZpk(credit.getCommunity().getZpks(), type))
         self.bound(credit, positionCost)
         self.updatePayments(credit)
         return self.saveDocument(credit)
@@ -40,8 +39,8 @@ class BankCreditManager(DocumentManager):
             if id == 0 and not remove:
                 position = self.initPosition(credit, '_payments_')
                 position.setType("BANK_CREDIT_PAYMENT")
-                position.setZpkCredit(self.findZpk(credit.getContractor().getZpks(), "CONTRACTOR"))
-                position.setZpkDebit(self.findZpk(credit.getCommunity().getZpks(), credit.getAttribute('CHARGE_TYPE')))
+                position.setCreditZpk(self.findZpk(credit.getContractor().getZpks(), "CONTRACTOR"))
+                position.setDebitZpk(self.findZpk(credit.getCommunity().getZpks(), credit.getAttribute('CHARGE_TYPE')))
                 self.bound(credit, position)
             if id != 0 and remove:
                 position = self.findById("DocumentPosition", id);
