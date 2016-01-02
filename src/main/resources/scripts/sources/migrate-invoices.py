f9293af843b877ad6895627c1ae80c8c178d6818
tomek <tomek@ADMINIB-2F10OB2>
2016-01-02 22:01:50 +0100

    .

--- src/main/resources/scripts/sources/migrate-invoices.py
+++ src/main/resources/scripts/sources/migrate-invoices.py
@@ -4,12 +4,12 @@ helpers.init(globals())
 from documents.migration.AccountProvision import AccountProvisionMigrator
 from documents.migration.BankNote import BankNoteMigrator
 from documents.migration.Invoice import InvoiceMigrator
-from documents.migration.Charging import ChargingMigrator
+from documents.migration.Charger import ChargerManager
 invoiceManager = InvoiceMigrator()
 #invoiceManager.migrateAll()
 noteManager = BankNoteMigrator()
 #noteManager.migrateAll()
 accountProvision = AccountProvisionMigrator()
 #accountProvision.migrateAll()
-charging = ChargingMigrator()
+charging = ChargerManager()
 charging.migrateAll()
