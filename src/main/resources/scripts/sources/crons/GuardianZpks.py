d3f356f195d1922709472fb00e312d23a432347e
tomek <tomek@ADMINIB-2F10OB2>
2016-01-04 23:14:59 +0100

    .

--- src/main/resources/scripts/sources/crons/GuardianZpks.py
+++ src/main/resources/scripts/sources/crons/GuardianZpks.py
@@ -3,11 +3,12 @@ from base.Container import Container
 class GuardianZpk(Container):
     
     def checkAll(self):
+        self._logger.info("Checking for wrongly calculated zpk balance...")
         for zpk in self.collect():
             self.checkZpk(zpk)
+        self._logger.info("Zpk balances checked.")    
             
     def checkZpk(self, zpk):
-        self._logger.info("Checking for wrongly calculated zpk balance...")
         balance = zpk.getCurrentBalance();
         calculatedCredit = self.sumCredit(zpk.getId()) + balance.getStartCredit()
         calculatedDebit = self.sumDebit(zpk.getId()) + balance.getStartDebit()
@@ -21,7 +22,6 @@ class GuardianZpk(Container):
             self._logger.info("On zpk %d found wrongly calculated debit:" % zpk.getId())
             self._logger.info("\\t calculated: %f" % calculatedDebit)
             self._logger.info("\\t expected: %f" % expectedDebit)
-        self._logger.info("Zpk balances checked.")    
             
     def sumCredit(self, zpkId):
         sql = "Select sum(e.value) From DocumentPosition e Where e.creditZpk.id = %d and e.booked = 1 and e.bookingPeriod.defaultPeriod = 1" % zpkId
@@ -29,7 +29,7 @@ class GuardianZpk(Container):
         if result == None:
             return 0
         else:
-            return result.floatValue()
+            return float(int(result.floatValue() * 100)) / 100
     
     def sumDebit(self, zpkId):
         sql = "Select sum(e.value) From DocumentPosition e Where e.debitZpk.id = %d and e.booked = 1 and e.bookingPeriod.defaultPeriod = 1" % zpkId
@@ -37,7 +37,7 @@ class GuardianZpk(Container):
         if result == None:
             return 0
         else:
-            return result.floatValue()
+            return float(int(result.floatValue() * 100)) / 100
             
     def collect(self):
         sql = "Select e From ZakladowyPlanKont e"
