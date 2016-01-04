737dc7e6aa6c8efea6a0c7740ebd854021b46a8c
tomek <tomek@ADMINIB-2F10OB2>
2016-01-04 22:21:17 +0100

    .

--- src/main/resources/scripts/sources/documents/PaymentRent.py
+++ src/main/resources/scripts/sources/documents/PaymentRent.py
@@ -8,24 +8,26 @@ class PaymentRentManager(DocumentManager):
         account = self.findById('Account', self._svars.get('accountId'))
         self._logger.info('Found account is type of %s' % account.getType().getKey())
         if account.getType().getKey() in ['RENT', 'DEFAULT']:
-            if account.getType().getKey() == 'RENT' and float(self._svars.get('rent')) != 0:
+            if float(self._svars.get('rent')) > 0:
                 value = float(self._svars.get('rent'))
             else:
                 value = float(self._svars.get('value'))
             if value != 0:
                 paymentPosition = self.initPosition(payment)
+                paymentPosition.setDescription(self._svars.get('title'))
                 paymentPosition.setClientName(self._svars.get('clientName'))
                 paymentPosition.setAccount(account)
                 paymentPosition.setCreditZpk(self.findZpk(payment.getPossession().getZpks(), 'POSSESSION'))
                 paymentPosition.setDebitZpk(self.findZpk(paymentPosition.getAccount().getZpks(), 'RENT', 'DEFAULT'))
                 self.bound(payment, paymentPosition)
         if account.getType().getKey() in ['REPAIR_FUND', 'DEFAULT']:
-            if account.getType().getKey() == 'REPAIR_FUND' and float(self._svars.get('repairFund')):
+            if float(self._svars.get('repairFund')) > 0:
                 value = float(self._svars.get('repairFund'))
             else:
                 value = float(self._svars.get('value'))
             if value != 0:
                 paymentPosition = self.initPosition(payment)
+                paymentPosition.setDescription(self._svars.get('title'))
                 paymentPosition.setClientName(self._svars.get('clientName'))
                 paymentPosition.setAccount(account)
                 paymentPosition.setCreditZpk(self.findZpk(payment.getPossession().getZpks(), 'POSSESSION_REPAIR_FUND'))
