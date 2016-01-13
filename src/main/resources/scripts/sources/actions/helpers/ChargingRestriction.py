9eacd39a5e0c19c17c928bf610387d9314294381
tomek <tomek@ADMINIB-2F10OB2>
2016-01-13 10:01:28 +0100

    .

--- src/main/resources/scripts/sources/actions/helpers/ChargingRestriction.py
+++ src/main/resources/scripts/sources/actions/helpers/ChargingRestriction.py
@@ -19,8 +19,8 @@ class ChargingRestriction(Restriction):
     def countChargings(self):
         currentMonth = BookingPeriodManager().getCurrentMonth()
         sql = "Select document"
-        sql += " From Document document Join document.positions position Join position.bookingPeriod bookingPeriod"
-        sql += (" Where bookingPeriod.defaultPeriod = 1 and position.month = %s and document.type = 'CHARGING'" % currentMonth)
+        sql += " From Document document"
+        sql += " Where document.type = 'CHARGING'"
         return len(self._entityManager.createQuery(sql).getResultList())
     
     def getTemplateName(self):
