69d1017385c4e94604ca33b2490c404810056ca0
tomek <tomek@ADMINIB-2F10OB2>
2016-01-13 09:59:39 +0100

    .

--- src/main/resources/scripts/sources/actions/helpers/ChargingRestriction.py
+++ src/main/resources/scripts/sources/actions/helpers/ChargingRestriction.py
@@ -21,7 +21,6 @@ class ChargingRestriction(Restriction):
         sql = "Select document"
         sql += " From Document document Join document.positions position Join position.bookingPeriod bookingPeriod"
         sql += (" Where bookingPeriod.defaultPeriod = 1 and position.month = %s and document.type = 'CHARGING'" % currentMonth)
-        sql += " Group By position.document"
         return len(self._entityManager.createQuery(sql).getResultList())
     
     def getTemplateName(self):
