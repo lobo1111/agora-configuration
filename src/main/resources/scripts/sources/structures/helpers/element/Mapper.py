c8587526bc492745c9a9034b0f26034236650e88
tomek <tomek@9.164.188.180>
2016-03-15 09:30:54 +0100

    ...

--- src/main/resources/scripts/sources/structures/helpers/element/Mapper.py
+++ src/main/resources/scripts/sources/structures/helpers/element/Mapper.py
@@ -10,11 +10,11 @@ class ElementMapper(Mapper):
     
     def initStructure(self):
         if int(self.get('id')) > 0:
-            self._logger.info("Element persist - it's an update. Found id: %s, of type" % (self.get('id'), self.get('type')))
+            self._logger.info("Element persist - it's an update. Found id: %s, of type %s" % (self.get('id'), self.get('type')))
             self._entity = self.loadEntity(self.get('type'), int(self.get('id')))
             self._isNew = False
         else:
-            self._logger.info("Account persist - it's a new account")
+            self._logger.info("Element persist - it's a new element")
             self._entity = self.initEntity(int(self.get('type')))
             self._isNew = True
             
