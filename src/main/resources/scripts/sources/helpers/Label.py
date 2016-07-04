43d6f5d3f908db904c42d77478e74db2450d1ea6
Tomasz Kopacki <tomasz@kopacki.eu>
2016-07-04 15:22:34 +0200

    ...

--- src/main/resources/scripts/sources/helpers/Label.py
+++ src/main/resources/scripts/sources/helpers/Label.py
@@ -3,6 +3,6 @@ from base.Container import Container
 class LabelManager(Container):
     
     def get(self, name):
-        value = uniode(self.findBy('Label', 'name', "'%s'" % name).getMessage()).encode('utf8')
+        value = unicode(self.findBy('Label', 'name', "'%s'" % name).getMessage()).encode('utf8')
         self._logger.info("Label %s resolved to %s" % (name, value))
         return value
