9a8236d893f7e499af0df040b8eba53bc204369c
Tomasz Kopacki <tomasz@kopacki.eu>
2016-07-04 14:36:26 +0200

    ...

--- src/main/resources/scripts/sources/reports/ZpksStatusReport.py
+++ src/main/resources/scripts/sources/reports/ZpksStatusReport.py
@@ -5,10 +5,12 @@ class ZpksStatusReport(Report):
     def obtainData(self):
         self._community = self.findById("Community", self._svars.get('communityId'))
         self._statusDate = self._svars.get('statusDate')
+        self._zpks = self._community.getZpks()
     
     def fillTemplate(self):
         self._context.put("community", self._community)
         self._context.put("statusDate", self._statusDate)
+        self._context.put("zpks", self._zpks)
         
     def getTemplateName(self):
         return "report-zpks-status"
