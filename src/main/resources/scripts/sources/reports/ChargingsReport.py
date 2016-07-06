cac32881d8f561d4c1068b7cb29c9c8506806d65
Tomasz Kopacki <tomasz@kopacki.eu>
2016-07-06 14:35:27 +0200

    ...

--- src/main/resources/scripts/sources/reports/ChargingsReport.py
+++ src/main/resources/scripts/sources/reports/ChargingsReport.py
@@ -40,7 +40,7 @@ class ChargingsReport(Report):
                 item['balance'] = balance
                 output.append(item)
                 processed.append(document)
-        output = output.sort(comparator)
+        output.sort(comparator)
         return output
     
     def getCreateDate(self, document):
