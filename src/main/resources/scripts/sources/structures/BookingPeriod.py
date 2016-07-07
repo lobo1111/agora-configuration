912edb9d5b0eb48665abdd81db6934667fa9cd9c
Tomasz Kopacki <tomasz@kopacki.eu>
2016-07-07 11:15:41 +0200

    ...

--- src/main/resources/scripts/sources/structures/BookingPeriod.py
+++ src/main/resources/scripts/sources/structures/BookingPeriod.py
@@ -27,7 +27,7 @@ class BookingPeriodManager(Container):
     
     def getCurrentMonthName(self):
         monthNumber = self.getCurrentMonth().getValue()
-        locale.setlocale(locale.LC_ALL, "pl_PL")
+        locale.setlocale(locale.LC_ALL, "polish")
         monthName = datetime.datetime.strptime(monthNumber, "%m").strftime("%B")
         locale.setlocale(locale.getdefaultlocale())
         return monthName
