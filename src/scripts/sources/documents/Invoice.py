27a3807d06d111a7a5feef3f9e6b451a9a1f6c23
Tomasz Kopacki <tomasz@kopacki.eu>
2016-08-30 13:39:51 +0200

    ...

--- src/scripts/sources/documents/Invoice.py
+++ src/scripts/sources/documents/Invoice.py
@@ -5,6 +5,12 @@ from java.math import BigDecimal
 class InvoiceManager(DocumentManager):
     _type = "INVOICE"
     
+    def persist(self):
+        if self.findById("Document", self._svars.get("id")) == '0':
+            self.create()
+        else:
+            self.update()
+    
     def create(self):
         invoice = self.initDocument(self._type)
         self.updateInvoiceData(invoice)
