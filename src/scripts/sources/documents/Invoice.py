f97b0e0bd550d34e43e67b92ea488ba0dd24441f
Tomasz Kopacki <tomasz@kopacki.eu>
2016-09-02 12:29:32 +0200

    ...

--- src/scripts/sources/documents/Invoice.py
+++ src/scripts/sources/documents/Invoice.py
@@ -59,7 +59,7 @@ class InvoiceManager(DocumentManager):
             if position.getType() == "INVOICE_COST" and not position.isCanceled():
                 value = value.add(position.getValue())
         #invoice.putAttribute("VALUE", str(value.setScale(2, RoundingMode.HALF_UP).floatValue()))
-        invoice.putAttribute("VALUE", '0.0')
+        invoice.putAttribute("PAYED", 'true')
         
     def checkIfPayed(self, invoice):
         costs = BigDecimal(0.0)
