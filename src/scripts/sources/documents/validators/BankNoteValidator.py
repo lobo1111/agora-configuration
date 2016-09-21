5f7d30236c5afcedc8c51fd4b6e63d28af5b1ef3
Tomasz Kopacki <tomasz@kopacki.eu>
2016-09-21 15:27:34 +0200

    ...

--- src/scripts/sources/documents/validators/BankNoteValidator.py
+++ src/scripts/sources/documents/validators/BankNoteValidator.py
@@ -6,7 +6,7 @@ class BankNoteValidator(Validator):
     
     def validate(self, document):
         self.check(document.getPossession(), [NotNoneValidator(messageParameter = self._label.get('validators.rentNote.possession'))])
-        self.check(document.getAttribute("ELEMENT_ID"), [NotNoneValidator(messageParameter = self._label.get('validators.rentNote.element'))])
+        self.check(document.getPositions().get(0).getAttribute("ELEMENT_ID"), [NotNoneValidator(messageParameter = self._label.get('validators.rentNote.element'))])
         self.check(payment.getValue().floatValue(), [DecimalValidator(messageParameter = self._label.get('validators.rentNote.value'))])
         
     
