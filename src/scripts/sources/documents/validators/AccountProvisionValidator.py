32680e4a981ffca6b0e79da519e0227fa7ca5533
Tomasz Kopacki <tomasz@kopacki.eu>
2016-09-22 09:44:24 +0200

    ...

--- src/scripts/sources/documents/validators/AccountProvisionValidator.py
+++ src/scripts/sources/documents/validators/AccountProvisionValidator.py
@@ -5,7 +5,7 @@ from structures.validators.common.DecimalValidator import DecimalValidator
 class AccountProvisionValidator(Validator):
     
     def validate(self, document):
-        self.check(document.getAccount(), [NotNoneValidator(messageParameter = self._label.get('field.accountNumber'))])
+        self.check(document.getPositions().get(0).getAccount(), [NotNoneValidator(messageParameter = self._label.get('field.accountNumber'))])
         self.check(document.getPositions().get(0).getValue().floatValue(), [DecimalValidator(messageParameter = self._label.get('field.value'))])
         
     
