91f8e1e374dbb85cb5133e5a37534f5114e1f65f
Tomasz Kopacki <tomasz@kopacki.eu>
2016-10-27 11:40:49 +0200

    ...

--- src/scripts/sources/documents/validators/PaymentRentValidator.py
+++ src/scripts/sources/documents/validators/PaymentRentValidator.py
@@ -6,13 +6,13 @@ from structures.validators.common.NotNoneValidator import NotNoneValidator
 class PaymentRentValidator(Validator):
     
     def validate(self, document):
+        self.check(document.getPossession(), [NotNoneValidator(messageParameter = self._label.get('field.possession'))])
         for position in document.getPositions():
             self.validatePosition(position)
         self.check(document.getPositions(), [NotEmptyDocumentValidator()])
         
     def validatePosition(self, position):
         self.check(position.getAccount(), [NotNoneValidator(messageParameter = self._label.get('field.account'))])
-        self.check(position.getPossession(), [NotNoneValidator(messageParameter = self._label.get('field.possession'))])
         self.check(position.getDescription(), [NotNoneValidator(messageParameter = self._label.get('field.description'))])
         self.check(position.getClientName(), [NotNoneValidator(messageParameter = self._label.get('field.clientName'))])
         self.check(position.getAttribute("CREATE_DATE").getValue(), [DateValidator(messageParameter = self._label.get('field.createDate'))])
